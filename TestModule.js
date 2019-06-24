e5560b6dd851fca13d2bc1e6383aa31399935923
davidj314 <davidj314@outlook.com>
2019-06-24 13:49:50 -0700

    Update TestModule.js

--- TestModule.js
+++ TestModule.js
@@ -1,15 +1,5 @@
-class Cards {
-  constructor() {
-    this.boop = "doop"
-  }
-
-  hello() {
-      console.log("In the module thingy hi!")
-  }
-}
-
-function goodbye(callback){
-  callback("hurrdurr");
-  console.log("woof woof");
+{Cards: hello() {
+        console.log("In the module thingy hi!")
+    }
 }
 
