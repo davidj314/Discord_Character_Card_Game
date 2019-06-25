f49d6cf77b8d85afe95ebc3605598136cc35e548
davidj314 <davidj314@outlook.com>
2019-06-24 22:02:41 -0700

    Update Cardington.js

--- Cardington.js
+++ Cardington.js
@@ -1,19 +1,37 @@
 const Canvas = require('canvas');
 var Discord = require('discord.js');
 module.exports = {
-	hello: function() {
-		console.log("did this work?");
+	
+	show_card: async function (url, up, down, left, right, callback){
+		const canvas = Canvas.createCanvas( 144, 180);
+		const ctx = canvas.getContext('2d');
+		ctx.strokeStyle = '#74037b';
+		ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+		const bck1 = await Canvas.loadImage('https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/blue0517.jpg?itok=V3825voJ');
+		const character = await Canvas.loadImage(url);
+		// Select the font size and type from one of the natively available fonts
+		ctx.font = '20px sans-serif';
+		// Select the style that will be used to fill the text in
+		ctx.fillStyle = '#ffffff';
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 1; 
+
+		ctx.drawImage(bck1, 0, 0, 144, 180);
+		ctx.drawImage(character, 3, 3, 138, 174);
+		ctx.strokeText(`  ${up} \n${left}  ${right}\n  ${down}`, 7, 22);
+		ctx.fillText(`  ${up} \n${left}  ${right}\n  ${down}`,  7, 22);
+		const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
+		callback(`Card`, attachment);		
 	},
 	
 	show_hand: async function(hand, nick, callback){
-		console.log("In show hand");
 	      	const canvas = Canvas.createCanvas( 745, 180);
       		const ctx = canvas.getContext('2d');
       		ctx.strokeStyle = '#74037b';
       		ctx.strokeRect(0, 0, canvas.width, canvas.height);
       		const bck1 = await Canvas.loadImage('https://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/blue0517.jpg?itok=V3825voJ');
       		const bck2 = await Canvas.loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ScbytNAjJFFGQRhGm-Me3ad-SJZbyzYm3A2FpU4MDsaao6D-');
-      		console.log("Loaded images?");
       		// Select the font size and type from one of the natively available fonts
       		ctx.font = '20px sans-serif';
       		// Select the style that will be used to fill the text in
@@ -34,7 +52,6 @@ module.exports = {
 
       		const attachment = new Discord.Attachment(canvas.toBuffer(), 'hand.png');
       		callback(`${nick}'s Hand`, attachment);	  
-		console.log("Returning from tester.show_hand");
 	},
    
 	show_board: async function (positions, callback){
@@ -413,7 +430,7 @@ module.exports = {
 
 
 
-
+//----------------------helper functions
 
 async function show_board (positions, callback){
       const canvas = Canvas.createCanvas(396, 418);
